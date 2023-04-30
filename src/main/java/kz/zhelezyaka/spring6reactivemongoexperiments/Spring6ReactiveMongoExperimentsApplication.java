c1d73bd9849cd4c3d815d3284783bc547340ee7d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-30 18:43:38 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/spring6reactivemongoexperiments/Spring6ReactiveMongoExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring6reactivemongoexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveMongoExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveMongoExperimentsApplication.class, args);
+    }
+
+}
