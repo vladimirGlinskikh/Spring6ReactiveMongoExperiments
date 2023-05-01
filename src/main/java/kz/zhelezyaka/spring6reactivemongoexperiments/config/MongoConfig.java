f00cbc530d569d7d5526122d17f52ba1891f69cf
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-01 10:13:26 +0300

    Mongo Database Authentication

--- src/main/java/kz/zhelezyaka/spring6reactivemongoexperiments/config/MongoConfig.java
+++ src/main/java/kz/zhelezyaka/spring6reactivemongoexperiments/config/MongoConfig.java
@@ -1,10 +1,17 @@
 package kz.zhelezyaka.spring6reactivemongoexperiments.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
+
+import static java.util.Collections.*;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -17,5 +24,15 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Override
     protected String getDatabaseName() {
         return "sfg";
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                        "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts(singletonList(
+                            new ServerAddress("127.0.0.1", 27017)));
+                });
     }
 }
