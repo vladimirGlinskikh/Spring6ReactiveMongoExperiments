e72c1be73986b7eb8cf6ae992ebe6682b0e5a553
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-13 18:26:21 +0300

    Spring Security Configuration

---
+++ src/main/java/kz/zhelezyaka/spring6reactivemongoexperiments/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package kz.zhelezyaka.spring6reactivemongoexperiments.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
